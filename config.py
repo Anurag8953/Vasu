97135e5795b6fefa326e05192d26bb7f4e6c2d69
Espro Repo <153180751+Esprorepo@users.noreply.github.com>
2024-01-14 18:41:04 +0530

    🝐꯭𐏓꯭꯭ ⃪⧉⃞🅴𝐒𝙿ⓇᎧ🦋⃝🅼𝐔𝚂Ⓘ𝙲ᥣ᭄𝄄꯭꯭꯭⟭♥️─⃛͢•༎𓆪

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Esprorepo/EsproMusicBot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ritik")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
